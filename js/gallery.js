6e62984130e43b629bc264913d1005ce99aa5e7a
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-02-28 21:44:11 -0500

    began image gallery

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+// declare variable for list of photos
+
+let photoGallery = [
+   "Man.jpg",
+   "Woods.jpg",
+   "Boat.jpg",
+   "Desert.jpg",
+];
+
+// declare variables for unique section with id
+
+let idPhoto = document.getElementById("gallery");
+
+// for loop that loops through photos
+
