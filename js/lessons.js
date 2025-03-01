936abfb050b2dafbd3f83d5011e48bdc585aff0c
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-02-28 19:20:24 -0500

    finished index.html and recreated score.html, gallery.html

---
+++ js/lessons.js
@@ -0,0 +1,23 @@
+// declare varaible for list of instruments
+
+const instrumentArray = [
+    "Piano",
+    "Guitar",
+    "Violin",
+    "Drums",
+    "Saxophone",
+    "Flute",
+];
+
+// make reference to the ul
+
+const ulInstrument = document.querySelector("body > ul");
+
+// for loop that loops through the instruments
+
+for (let index = 0; index < instrumentArray.length; index++) {
+    const instrumentArrays = instrumentArray[index];
+    console.log(instrumentArrays);
+    ulInstrument.innerHTML+="<li class='instruments' >" + instrumentArrays + "</li>";
+
+}
